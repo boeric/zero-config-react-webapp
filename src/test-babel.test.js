37b325ed78c7b73871534df41fe6042461bbd027
Bo Ericsson <bo@boe.net>
2020-07-29 10:46:52 -0700

    Support class fields (experimental feature)

--- src/test-babel.test.js
+++ src/test-babel.test.js
@@ -6,3 +6,41 @@ import testBabel from './test-babel';
 test('string from default export', () => {
   expect(testBabel()).toMatch('default export/import works');
 });
+
+// Test public and private class field declarations (currently an experimental feature)
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
+const PUBLIC_FIELD_VALUE = 'valueOfAPublicField';
+const PRIVATE_FIELD_VALUE = 'valueOfAPrivateField';
+
+class Test {
+  /* eslint-disable lines-between-class-members */
+  aPublicField = PUBLIC_FIELD_VALUE;
+  #aPrivateField = PRIVATE_FIELD_VALUE;
+  /* eslint-enable lines-between-class-members */
+
+  get publicField() {
+    return this.aPublicField;
+  }
+
+  get privateField() {
+    return this.#aPrivateField;
+  }
+}
+
+const testInstance = new Test();
+
+test('access public class field with getter', () => {
+  expect(testInstance.publicField).toEqual(PUBLIC_FIELD_VALUE);
+});
+
+test('access public class field directly', () => {
+  expect(testInstance.aPublicField).toEqual(PUBLIC_FIELD_VALUE);
+});
+
+test('access private class field with getter', () => {
+  expect(testInstance.privateField).toEqual(PRIVATE_FIELD_VALUE);
+});
+
+test('access public class field directly', () => {
+  expect(testInstance.aPrivateField).toEqual(undefined);
+});
