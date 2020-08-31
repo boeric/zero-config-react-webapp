2113b28a67b14a343de6abc8d12215db2f2bf25a
Bo Ericsson <bo@boe.net>
2020-08-30 20:51:44 -0700

    Added d3v6

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@
 import 'regenerator-runtime/runtime';
 import './styles.css';
 
+// Imports
+import * as d3 from 'd3';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
@@ -13,6 +15,9 @@ import App from './App';
 
 // Test various ES6/ES-2018 features
 console.log(testBabel());
+
+// Log D3 version
+console.log(`D3 version: ${d3.version}`);
 
 // Initial React render
 const title = 'Zero Config React Webapp Template';
